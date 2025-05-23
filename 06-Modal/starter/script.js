a5bfc19203d0df25b47d37c664c9175dacd1e559
haramsong <alakazam1324@gmail.com>
2025-05-23 15:27:40 +0900

    ✨ 6장 클리어

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// querySelector -> Multiple이면 첫 번째(한 개)만 선택됨
+// const btnsOpenModal = document.querySelector('.show-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  // . 을 쓸때는 Selector에만 해당 됨.
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+})
